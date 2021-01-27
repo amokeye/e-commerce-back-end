889cb58bf50807c96b542737d473c67f45f9d494
amokeye <67798512+amokeye@users.noreply.github.com>
2021-01-26 19:44:58 -0800

    updated tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,79 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+// GET all tags and include associated Product data
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll(
+    {
+      include: {
+        model: Product
+      }
+    }).then(tagData => {
+      res.json(tagData);
+    }).catch(error => {
+      res.status(500).json(error);
+    });
 });
 
+// GET tag by 'id' and include associated Product data
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product
+    }
+  }).then(tagById => {
+    res.json(tagById);
+  }).catch(error => {
+    res.status(500).json(error);
+  });
 });
 
+// POST new tag
 router.post('/', (req, res) => {
-  // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  }).then(newTag => {
+    res.json(newTag);
+  }).catch(error => {
+    res.status(500).json(error);
+  });
 });
 
+// Update/PUT new tag's name by 'id'
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(tagToUpdate => {
+    if (!tagToUpdate) {
+      res.status(404).json({ message: 'No tag with this ID exists in the database.' });
+      return;
+    }
+    res.json(tagToUpdate);
+  }).catch(error => {
+    res.status(500).json(error);
+  });
 });
 
+// DELETE tab by 'id'
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(tagToDelete => {
+    if (!tagToDelete) {
+      res.status(404).json({ message: 'No tag with this ID exists in the database.' });
+      return;
+    }
+    res.json(tagToDelete);
+  }).catch(error => {
+    res.status(500).json(error);
+  });
 });
 
 module.exports = router;
